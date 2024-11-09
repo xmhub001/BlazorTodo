4d82f0ff22d4e7512c32e6c8d03e89ee9805966b
lowell mitchell <lmitchell001@hotmail.com>
2024-11-09 10:23:42 -0700

    Add project files.

---
+++ BlazorTodo/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace BlazorTodo
+{
+    public class TodoItem
+    {
+        public int Id { get; set; } 
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
